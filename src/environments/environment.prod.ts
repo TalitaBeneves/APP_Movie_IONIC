21f6b252aae1877d7540c65206ad37abc8ce42bc
Talita Beneves <talitabeneves15@gmail.com>
2022-06-17 20:27:57 -0300

    Key do firebase e tmdb

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'filmes-ba01d',
+    appId: '1:1021248886206:web:23623bf67b74c007a255ef',
+    storageBucket: 'filmes-ba01d.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAdSqpzVHMK2_FBI1sFt9gFDtfWnr30LHc',
+    authDomain: 'filmes-ba01d.firebaseapp.com',
+    messagingSenderId: '1021248886206',
+    measurementId: 'G-HD9D96XNYX',
+  },
+  production: true,
+  apiKey: 'e05d1b0608431d079172111d414d07e4',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'filmes-ba01d',
+    appId: '1:1021248886206:web:23623bf67b74c007a255ef',
+    storageBucket: 'filmes-ba01d.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAdSqpzVHMK2_FBI1sFt9gFDtfWnr30LHc',
+    authDomain: 'filmes-ba01d.firebaseapp.com',
+    messagingSenderId: '1021248886206',
+    measurementId: 'G-HD9D96XNYX',
+  },
+  production: false,
+  apiKey: 'e05d1b0608431d079172111d414d07e4',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
