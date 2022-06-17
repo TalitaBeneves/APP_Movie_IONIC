8c006e4f1afaf8fc92b3fafbe87abdfdc339b12f
Talita Beneves <talitabeneves15@gmail.com>
2022-06-17 20:25:03 -0300

    Criando autenticação de login e cadastro com firebase

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from '@angular/fire/auth';
+import { signOut } from '@firebase/auth';
+import { AlertController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private auth: Auth, private alertController: AlertController) {}
+
+  async register({ email, password }) {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        this.auth,
+        email,
+        password
+      );
+      return user;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      const user = await signInWithEmailAndPassword(this.auth, email, password);
+      return user;
+    } catch (error) {
+      return null;
+    }
+  }
+
+  logout() {
+    return signOut(this.auth);
+  }
+
+  async showAlert(header, message) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+}
